fb6c423f9746f960ae5fe43823b7e449a0c2a454
weiaijie <weiaijie@Outlook.com>
2018-12-10 16:41:45 +0800

    添加vuex插件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
@@ -6,7 +7,7 @@ import VueAxios from 'vue-axios'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-
+Vue.use(Vuex)
 Vue.use(ElementUI)
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
@@ -14,6 +15,7 @@ Vue.config.productionTip = false
 axios.defaults.timeout = 5000;                        //响应时间
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';           //配置请求头
 axios.defaults.baseURL = '/api';   //配置接口地址
+
 
 new Vue({
   router,
